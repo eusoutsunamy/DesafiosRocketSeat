900353e94f79e6933e517fbc6d2e24b886e50729
jose pio <eusoutsunamy@gmail.com>
2020-07-02 17:07:06 -0300

    Desafio: Conceitos do Node.js

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+/*
+ funcao para fazer log da api
+*/
+function logRequests(request, response, next){
+    const { method, url } = request;
+    console.log(method);
+    console.log(url);
+    return next();
+}
+
+function validRequest(request, response, next){
+    const { id } = request.params;
+    if(id <= 0){
+        return response.status(400).json({error: 'Id inválido.'});
+    }
+    return next();
+}
+
+//app.use(logRequests);
+app.use('/projects/:id', validRequest);
+
+app.get('/projects', (request, response) => {
+    const query = request.query;
+
+    return response.json(['teste 1', 'teste 2']);
+})
+
+app.post('/projects', (request, response) => {
+    const body = request.body;
+    console.log(body);
+    return response.json(['teste 1', 'teste 2', 'teste 3']);
+})
+
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    console.log(id);
+    return response.json(['teste 1', 'teste 4', 'teste 3']);
+})
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json(['teste 1', 'teste 4']);
+})
+
+app.listen(3333, () => {
+    console.log('✌  Back-end started!');
+});
+
---
+++ src/repositories.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const {uuid, isUuid } = require('uuidv4');
+const app = express();
+
+app.use(express.json());
+
+const repositories= [];
+
+function validRequest(request, response, next){
+    const { id } = request.params;
+    if(!isUuid(id)){
+        return response.status(400).json({error: 'Invalid repository Id.'});
+    }
+    return next();
+}
+
+app.use('/repositories/:id', validRequest);
+
+app.get('/repositories', (request, response) => {
+    return response.json(repositories);
+})
+
+app.post('/repositories', (request, response) => {
+    const {title, url, techs} = request.body;
+    var repository = { id: uuid() , title, url, techs, likes: 0 }; 
+    repositories.push(repository);
+    return response.json(repository);
+})
+
+app.post('/repositories/:id/like', (request, response) => {
+    const { id } = request.params;
+    const {title, url, techs} = request.body;
+
+    const indexIten = repositories.findIndex( repos => repos.id === id);
+    if(indexIten < 0){
+        return response.status(400).json({error: 'Repository Id not founded.'});
+    }
+    const repository = repositories[indexIten];
+    repository.likes = repository.likes + 1;
+    repositories[indexIten] = repository;
+
+    return response.json(repository);
+})
+
+app.put('/repositories/:id', (request, response) => {
+
+    const { id } = request.params;
+    const {title, url, techs} = request.body;
+
+    const indexIten = repositories.findIndex( repos => repos.id === id);
+    if(indexIten < 0){
+        return response.status(400).json({error: 'Repository Id not founded.'});
+    }
+    const repository = repositories[indexIten];
+    repository.title = title;
+    repository.url = url;
+    repository.techs = techs;
+    repositories[indexIten] = repository;
+
+    return response.json(repository);
+})
+
+app.delete('/repositories/:id', (request, response) => {
+    const { id } = request.params;
+
+    const indexIten = repositories.findIndex( repos => repos.id === id);
+    if(indexIten < 0){
+        return response.status(400).json({error: 'Repository Id not founded.'});
+    }
+    repositories.splice(indexIten, 1);
+    response.status(204).send();
+})
+
+app.listen(3333, () => {
+    console.log('✌  Back-end started!');
+});
+
